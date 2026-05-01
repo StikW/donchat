6aafb3d3d17159f625eebfb45eb5d58e6ca7811f
StikW <danieldg10122016@gmail.com>
2026-05-01 15:50:38 -0500

    puerto

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { getPool } from "./db/pool.js";
 import { productsRouter } from "./routes/products.js";
 
 const app = express();
-const PORT = process.env.PORT ?? 3001;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 
